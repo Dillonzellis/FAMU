5a6564411a6adca17b500156389adef3f6369872
Dillon Ellis <dellis@fcul.com>
2022-05-18 15:15:05 -0400

    updated contact page map w hyperlink

--- contact-us.php
+++ contact-us.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
@@ -6,268 +5,281 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js">
 <!--<![endif]-->
+
 <head>
-<title>FAMU FCU - Contact Us</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-<meta name="description" content="">
-<link rel="shortcut icon" href="../favicon.ico">
+    <title>FAMU FCU - Contact Us</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="../favicon.ico">
 
-<!-- CSS includes //-->
-<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="../css/normalize.css" type="text/css">
-<link rel="stylesheet" href="../css/colorbox-skins/4/colorbox.css" type="text/css">
-<link rel="stylesheet" href="../css/animate.css" type="text/css">
-<link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-<link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css" type="text/css">
-<link rel="stylesheet" href="../css/colorbox-skins/4/colorbox.css" type="text/css">
-<link rel="stylesheet" href="../css/helpers.css" type="text/css">
-<link rel="stylesheet" href="../css/component.css" type="text/css">
+    <!-- CSS includes //-->
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="../css/colorbox-skins/4/colorbox.css" type="text/css">
+    <link rel="stylesheet" href="../css/animate.css" type="text/css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/colorbox-skins/4/colorbox.css" type="text/css">
+    <link rel="stylesheet" href="../css/helpers.css" type="text/css">
+    <link rel="stylesheet" href="../css/component.css" type="text/css">
 
-<!-- LayerSlider styles -->
-<link rel="stylesheet" href="../css/layerslider/css/layerslider.css" type="text/css">
+    <!-- LayerSlider styles -->
+    <link rel="stylesheet" href="../css/layerslider/css/layerslider.css" type="text/css">
 
-<link rel="stylesheet" href="../css/colors/1.css" type="text/css" id="site-color">
-	
-<!-- Remodal styles -->
+    <link rel="stylesheet" href="../css/colors/1.css" type="text/css" id="site-color">
+
+    <!-- Remodal styles -->
     <link rel="stylesheet" href="../remodal/dist/remodal.css">
     <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
 
-<!-- JS includes //-->
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries //-->
-<!--[if lt IE 9]>
+    <!-- JS includes //-->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries //-->
+    <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-	
-	<style>
-		.img-shadow {
-			border:2px solid #fff;
-			-moz-box-shadow: 10px 10px 5px #ccc;
-  -webkit-box-shadow: 10px 10px 5px #ccc;
-  box-shadow: 10px 10px 5px #ccc;
-  -moz-border-radius:25px;
-  -webkit-border-radius:25px;
-  border-radius:25px;
-		}
-		
-table.ratesTable {
-  border: 1px solid #EEEEEE;
-  width: 100%;
-  border-collapse: collapse;
-}
-table.ratesTable td, table.ratesTable th {
-  border: 1px solid #AAAAAA;
-  padding: 3px 2px;
-}
-table.ratesTable tr:nth-child(even) {
-  background: #8AB68C;
-}
-table.ratesTable thead {
-  background: #419143;
-  border-bottom: 2px solid #444444;
-}
-table.ratesTable thead th {
-  font-weight: bold;
-  color: #000000;
-  text-align: center;
-}
-table.ratesTable tfoot td {
-  font-size: 14px;
-}
-table.ratesTable tfoot .links {
-  text-align: right;
-}
-table.ratesTable tfoot .links a{
-  display: inline-block;
-  background: #1C6EA4;
-  color: #FFFFFF;
-  padding: 2px 8px;
-  border-radius: 5px;
-}
-	</style>
+
+    <style>
+        .img-shadow {
+            border: 2px solid #fff;
+            -moz-box-shadow: 10px 10px 5px #ccc;
+            -webkit-box-shadow: 10px 10px 5px #ccc;
+            box-shadow: 10px 10px 5px #ccc;
+            -moz-border-radius: 25px;
+            -webkit-border-radius: 25px;
+            border-radius: 25px;
+        }
+
+        table.ratesTable {
+            border: 1px solid #EEEEEE;
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table.ratesTable td,
+        table.ratesTable th {
+            border: 1px solid #AAAAAA;
+            padding: 3px 2px;
+        }
+
+        table.ratesTable tr:nth-child(even) {
+            background: #8AB68C;
+        }
+
+        table.ratesTable thead {
+            background: #419143;
+            border-bottom: 2px solid #444444;
+        }
+
+        table.ratesTable thead th {
+            font-weight: bold;
+            color: #000000;
+            text-align: center;
+        }
+
+        table.ratesTable tfoot td {
+            font-size: 14px;
+        }
+
+        table.ratesTable tfoot .links {
+            text-align: right;
+        }
+
+        table.ratesTable tfoot .links a {
+            display: inline-block;
+            background: #1C6EA4;
+            color: #FFFFFF;
+            padding: 2px 8px;
+            border-radius: 5px;
+        }
+    </style>
 
 </head>
 
 <body>
 
-	<!-- Wrapping all content //-->
+    <!-- Wrapping all content //-->
 
 
-	<div id="wrap-all">
+    <div id="wrap-all">
 
-		<!-- Site header start //-->
+        <!-- Site header start //-->
 
-		<header class="site-header" id="site-header">
+        <header class="site-header" id="site-header">
 
-			<?php include '../inc/header.php';?>
+            <?php include '../inc/header.php'; ?>
 
-		</header>
+        </header>
 
-		<!-- Site header end //-->
+        <!-- Site header end //-->
 
-		<!-- Title bar start //-->
+        <!-- Title bar start //-->
 
-		<section class="title-bar bg-img-dark transparent-bg-secondary">
+        <section class="title-bar bg-img-dark transparent-bg-secondary">
 
-			<div class="container">
+            <div class="container">
 
-				<div class="row">
+                <div class="row">
 
-					<div class="col-lg-12">
+                    <div class="col-lg-12">
 
-						<h1>Contact Us</h1>
+                        <h1>Contact Us</h1>
 
-					</div>
+                    </div>
 
-				</div>
+                </div>
 
-			</div>
+            </div>
 
-		</section>
+        </section>
 
-		<!-- Title bar end //-->
+        <!-- Title bar end //-->
 
-		<!-- Content start //-->
+        <!-- Content start //-->
 
-		<main class="site-main">
+        <main class="site-main">
 
-			
-<!-- Text start //-->
 
-			<section class="secondary-bg-lighten border-bottom-1">
+            <!-- Text start //-->
 
-				<div class="container">
+            <section class="secondary-bg-lighten border-bottom-1">
 
-					<div class="row">
-                        
+                <div class="container">
+
+                    <div class="row">
+
                         <div class="col-lg-3">
 
-							<h3>office location</h3>
-							
-							<p>1610 South Monroe Street<br>               
-                               Tallahassee, FL 32301</p>
-                            
-                            <p>Phone: 850.222.4151<br> 
-                               Fax: 850.222.5401</p>
-                            
+                            <h3>office location</h3>
+
+                            <p>1610 South Monroe Street<br>
+                                Tallahassee, FL 32301</p>
+
+                            <p>Phone: 850.222.4151<br>
+                                Fax: 850.222.5401</p>
+
                             <p>Lobby by appointment only.</p>
-                            
+
                             <p>Drive Thru Hours:<br>
-                               Monday - Friday 8:30 a.m. - 5:00 p.m.</p>
-				              </div>
-                        
-                        
+                                Monday - Friday 8:30 a.m. - 5:00 p.m.</p>
+                        </div>
+
+
                         <div class="col-lg-3">
 
-							<h3>Credit Card Member Service</h3>
+                            <h3>Credit Card Member Service</h3>
                             <p> Phone: 888.415.6154</p>
-							
-							<p>Debit Card Lost/Stolen<br>               
-                               Phone: 800.472.3272</p>
-                            
-                            <p>Bill Pay Member Service<br> 
-                               Phone: 800.877.8021 <br> 7:00 AM - 1:00 AM ET<br> 
+
+                            <p>Debit Card Lost/Stolen<br>
+                                Phone: 800.472.3272</p>
+
+                            <p>Bill Pay Member Service<br>
+                                Phone: 800.877.8021 <br> 7:00 AM - 1:00 AM ET<br>
                                 7 days a week</p>
-                            
-						</div>
-						
-						<div class="col-lg-6">
 
-							<figure class="margin-top-0">
+                        </div>
 
-								<img src="../img/contact-us.png" alt="Contact Us" class="img-shadow img-responsive">
+                        <div class="col-lg-6">
 
-							</figure>
-							
-						</div>
+                            <figure class="margin-top-0">
 
-					</div>
-                    
-                    
-                 <p class="margin-top-40"><a href="../pages/holiday-hours.php" class="biss-btn biss-btn-primary">holiday hours</a></p>
+                                <a href="https://www.google.com/maps/place/Florida+A+%26+M+University+Federal+Credit+Union/@30.4266112,-84.2831889,17z/data=!4m13!1m7!3m6!1s0x88ecf568050ad54b:0xdbb54fa56a4a18bf!2s1610+S+Monroe+St,+Tallahassee,+FL+32301!3b1!8m2!3d30.4266112!4d-84.2810002!3m4!1s0x88ecf5680f8cdb91:0xf56d7fbe85aee186!8m2!3d30.42659!4d-84.281029" target="_blank" rel="noopener noreferrer"><img src="../img/contact-us.png" alt="Contact Us" class="img-shadow img-responsive"></a>
 
-			  </div>
+                            </figure>
 
-			</section>
+                        </div>
 
-			<!-- Text end //-->
-            
-            
-			
-			
-			
-			<!-- Text start //-->
+                    </div>
 
-			<section class="border-bottom-1">
 
-				<div class="container">
+                    <p class="margin-top-40"><a href="../pages/holiday-hours.php" class="biss-btn biss-btn-primary">holiday hours</a></p>
 
-					<div class="row">
-						
-						
+                </div>
 
-						<div class="col-lg-12">
-							
-						
-							
-							<p class="margin-top-40"><!-- form.123formbuilder.com script begins here --><script type="text/javascript" defer src="https://form.123formbuilder.com/embed/6092400.js" data-role="form" data-default-width="650px"></script><!-- form.123formbuilder.com script ends here --></p>
+            </section>
 
-						</div>
+            <!-- Text end //-->
 
-					</div>
 
-				</div>
 
-			</section>
 
-			<!-- Text end //-->
-			
-			
 
-		</main>
+            <!-- Text start //-->
 
-		<!-- Content end //-->
+            <section class="border-bottom-1">
 
-		<!-- Site footer start //-->
+                <div class="container">
 
-		<footer class="site-footer">
+                    <div class="row">
 
-			<?php include '../inc/footer.php';?>
 
-		</footer>
 
-		<!-- Site footer end //-->
+                        <div class="col-lg-12">
 
-		<!-- Scroll to the top button start //-->
 
-		<div id="scroll-to-the-top">
 
-			<i class="fa fa-angle-up"></i>
+                            <p class="margin-top-40">
+                                <!-- form.123formbuilder.com script begins here -->
+                                <script type="text/javascript" defer src="https://form.123formbuilder.com/embed/6092400.js" data-role="form" data-default-width="650px"></script><!-- form.123formbuilder.com script ends here -->
+                            </p>
 
-		</div>
+                        </div>
 
-		<!-- Scroll to the top button end //-->
+                    </div>
 
-	</div>
+                </div>
 
-<!-- JS includes //-->
-<script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-<script src="../js/vendor/jquery-1.11.1.min.js"></script>
-<script src="../js/vendor/bootstrap.min.js"></script>
-<script src="../js/imagesloaded.pkgd.min.js"></script>
-<script src="../js/masonry.pkgd.min.js"></script>
-<script src="../js/less-1.7.4.min.js"></script>
-<script src="../js/jquery.colorbox-min.js"></script>
-<script src="../js/jquery.easing.1.3.js"></script>
-<script src="../js/jquery.rivathemes.js"></script>
-<script src="../js/biss.js"></script>
-	
-<?php include '../inc/modals.php';?>
-<script src="../remodal/dist/remodal.js"></script>
+            </section>
+
+            <!-- Text end //-->
+
+
+
+        </main>
+
+        <!-- Content end //-->
+
+        <!-- Site footer start //-->
+
+        <footer class="site-footer">
+
+            <?php include '../inc/footer.php'; ?>
+
+        </footer>
+
+        <!-- Site footer end //-->
+
+        <!-- Scroll to the top button start //-->
+
+        <div id="scroll-to-the-top">
+
+            <i class="fa fa-angle-up"></i>
+
+        </div>
+
+        <!-- Scroll to the top button end //-->
+
+    </div>
+
+    <!-- JS includes //-->
+    <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src="../js/vendor/jquery-1.11.1.min.js"></script>
+    <script src="../js/vendor/bootstrap.min.js"></script>
+    <script src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/masonry.pkgd.min.js"></script>
+    <script src="../js/less-1.7.4.min.js"></script>
+    <script src="../js/jquery.colorbox-min.js"></script>
+    <script src="../js/jquery.easing.1.3.js"></script>
+    <script src="../js/jquery.rivathemes.js"></script>
+    <script src="../js/biss.js"></script>
+
+    <?php include '../inc/modals.php'; ?>
+    <script src="../remodal/dist/remodal.js"></script>
 </body>
+
 </html>
