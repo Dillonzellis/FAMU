bf5474d29fd4c95e7de7cfa1b00b6cbc893c9c8e
Dillon Ellis <dellis@fcul.com>
2022-07-20 10:11:36 -0400

    original index page

---
+++ index.php
@@ -0,0 +1,495 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]>html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<title>FAMU</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Welcome to FAMU Federal Credit Union, home for all of your personal and business financial needs in Tallahassee Florida.">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+<link rel="shortcut icon" href="favicon.ico">
+
+<!-- CSS includes //-->
+<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/normalize.css" type="text/css">
+<link rel="stylesheet" href="css/colorbox-skins/4/colorbox.css" type="text/css">
+<link rel="stylesheet" href="css/animate.css" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="css/colorbox-skins/4/colorbox.css" type="text/css">
+<link rel="stylesheet" href="css/helpers.css" type="text/css">
+<link rel="stylesheet" href="css/component.css" type="text/css">
+
+<!-- LayerSlider styles -->
+<link rel="stylesheet" href="css/layerslider/css/layerslider.css" type="text/css">
+
+<link rel="stylesheet" href="css/colors/1.css" type="text/css" id="site-color">
+
+<!-- Remodal styles -->
+    <link rel="stylesheet" href="remodal/dist/remodal.css">
+    <link rel="stylesheet" href="remodal/dist/remodal-default-theme.css">
+<!-- JS includes //-->
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries //-->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+	
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','//connect.facebook.net/en_US/fbevents.js');
+	
+	fbq('init', '785937914872218');
+	fbq('track', "PageView");</script>
+	
+	<!-- End Facebook Pixel Code -->
+
+</head>
+
+<body>	
+
+	
+	<!-- Wrapping all content //-->
+
+
+	<div id="wrap-all">
+
+		<!-- Site header start //-->
+
+		<header class="site-header" id="site-header">
+
+			<?php include 'inc/header.php';?>
+
+		</header>
+
+		<!-- Site header end //-->
+
+		<!-- Layerslider start //-->
+
+		<section class="padding-top-0 padding-bottom-0">
+
+			<div id="layerslider" class="width-100 float-left layer-slider">
+				
+				
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/mobilebanking.png" alt="Mobile Banking Tutorial">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">Mobile Banking Tutorial </p>
+
+		            	<p class="description">Manage your money anytime, anywhere with Mobile Banking</p>
+
+		            	<p><a href="pages/mobile-banking-tutorial.php" class="biss-btn biss-btn-primary">learn more</a></p>
+
+	              </div>
+
+				</div>	
+				
+				
+				
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/annual-meeting.png" alt="Annual Meeting">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">FAMU FCU 87th Annual Meeting </p>
+
+		            	<p class="description">Was Held On May 10, 2022 </p>
+
+		            	<p><a href="pages/annual-meeting.php" class="biss-btn biss-btn-primary">learn more</a></p>
+
+	              </div>
+
+				</div>
+				
+				
+				
+				
+				
+					<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/mortgage.png" alt="Mortgages">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	<p class="subtitle">Purchase or Refinance with Us!</p>
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">Need a Mortgage Loan?</p>
+
+		            	<p class="description">WE'VE GOT YOU COVERED</p>
+
+		            	<p><a href="#" data-remodal-target="mortgage-loan-programs" class="biss-btn biss-btn-primary">apply now</a></p>
+
+	              </div>
+
+				</div>
+				
+				
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/ppp-forgiveness.png" alt="PPP Forgiveness Assistance">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	<p class="subtitle">LIVE ZOOM</p>
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">PPP LOAN FORGIVINESS ASSISTANCE </p>
+
+		            	<p class="description">EVERY THURSDAY 6:30 PM - 7:30 PM</p>
+
+		            	<p><a href="pages/ppp-forgiveness-zoom.php"  class="biss-btn biss-btn-primary">learn more</a></p>
+
+	              </div>
+
+				</div>
+				
+				
+				
+				
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/smartsteps.jpg" alt="SmartSteps Micro-Business Loan">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	<p class="subtitle">BUSINESS LENDING LOAN PROGRAM</p>
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">SmartSteps</p>
+
+		            	<p class="description">Supporting Small Businesses</p>
+
+		            	<p><a href="pages/smartsteps.php" class="biss-btn biss-btn-border-primary">apply now</a></p>
+
+	              </div>
+
+				</div>
+				
+				
+			
+				
+				
+
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/banner/test1.jpg" alt="layer1-background">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	<p class="subtitle">Bring in the New Year with our...</p>
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">Auto Loan Special*</p>
+
+		            	<p class="description">Hot Rates | No Payments for 60 Days** | Fast & easy application</p>
+						
+						<p class="description" style="font-size: 12px;">*All Loans are subject to approval. **No payment for 60 days, interest will accrue on the loan beginning the date of disbursement. Promotion applies to new FAMU FCU loans only. See Credit Union for full details.</p>
+
+		            	<p><a href="pages/auto-loans.php" class="biss-btn biss-btn-border-primary">learn more</a></p>
+
+	              </div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Layerslider end //-->
+
+		<!-- Action start //-->
+
+		<section>
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-lg-12">
+
+						<div class="action-block-1">
+
+							<div class="width-66 float-left">
+
+								<p class="h">become a <span class="primary-color">member</span> of famu fcu today!</p>
+
+								<p>Your membership entitles you to a vast array of financial services and privileges with the Credit Union.</p>
+
+							</div>
+
+							<div class="width-33 float-right">
+
+								<a href="pages/join-now.php" style="color:#000;" class="biss-btn biss-btn-primary width-50 float-right sm-width-100 md-width-100">join today!</a>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Action end //-->
+
+		<!-- Services #1 start //-->
+
+		<section class="padding-bottom-30">
+
+			<div class="container">
+
+				<div class="row">
+					
+					<!-- Services #1 item start //-->
+
+					<div class="col-lg-4 col-md-4">
+
+						<article class="services-1">
+
+							<span class="icon"><i class="fa fa-bank"></i></span>
+
+							<div class="inner">
+
+								<!-- Service title //-->
+
+								<p class="h">Mortgages</p>
+
+								<!-- Service description //-->
+
+								<p style="font-size: 14px;">Click below to apply for a mortgage loan.</p>
+
+								<!-- Service button //-->
+
+								<p class="b"><a href="#" data-remodal-target="mortgage-loan-programs" class="biss-btn biss-btn-primary">apply now</a></p>
+
+							</div>
+
+							<div class="color-bg"></div>
+
+						</article>
+
+					</div>
+
+					<!-- Services #1 item end //-->
+
+					<!-- Services #2 item start //-->
+
+					<div class="col-lg-4 col-md-4">
+
+						<article class="services-1">
+
+							<span class="icon"><i class="fa fa-cubes"></i></span>
+
+							<div class="inner">
+
+								<!-- Service title //-->
+
+								<p class="h">Apply for a Loan</p>
+
+								<!-- Service description //-->
+
+								<p style="font-size: 14px;">See how we can fulfull your financial needs.</p>
+
+								<!-- Service button //-->
+
+								<p class="b"><a href="https://www.loanliner.com/LoanRequest/CLPPresenter/LoanList.aspx?CUID=00904355&LoanListId=2124&ChannelId=4799020706278145602&LocationId=9854675806270194221&IsFramed=F" target="_blank" class="biss-btn biss-btn-border-primary">apply now</a></p>
+
+							</div>
+
+							<div class="color-bg"></div>
+
+						</article>
+
+					</div>
+
+					<!-- Services #2 item end //-->
+
+					<!-- Services #3 item start //-->
+
+					<div class="col-lg-4 col-md-4">
+
+						<article class="services-1">
+
+							<span class="icon"><i class="fa fa-empire"></i></span>
+
+							<div class="inner">
+
+								<!-- Service title //-->
+
+								<p class="h">Micro-Lending Program</p>
+
+								<!-- Service description //-->
+
+								<p style="font-size: 14px;">Supporting small businesses</p>
+
+								<!-- Service button //-->
+
+								<p class="b"><a href="pages/smartsteps.php" class="biss-btn biss-btn-border-primary">learn more</a></p>
+
+							</div>
+
+							<div class="color-bg"></div>
+
+						</article>
+
+					</div>
+
+					<!-- Services #3 item end //-->
+
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Services #1 end //-->
+		
+		<!-- Layerslider start //-->
+
+		<section class="padding-top-0 padding-bottom-0">
+
+			<div id="layerslider2" class="width-100 float-left layer-slider">
+
+				<div class="ls-layer">
+
+					<img class="ls-bg" src="img/btm-banner/test1.jpg" alt="layer1-background">
+
+		            <div class="ls-s2 slide-description" style="top: 50px; slidedirection: fade; scalein: 2; transition2d: all; slidedelay: 6000; delayin: 1000; delayout: 1000; durationin: 1000; easingin: easeOutExpo;">
+
+		            	<p class="subtitle">Need a new ride? We can help...</p>
+
+		            	<div class="cl"></div>
+
+		            	<p class="title">Auto Loan Special*</p>
+
+		            	<p class="description">Hot Rates | No Payments for 60 Days** | Fast & easy application</p>
+						
+						<p class="description" style="font-size: 12px;">*All Loans are subject to approval. **No payment for 60 days, interest will accrue on the loan beginning the date of disbursement. Promotion applies to new FAMU FCU loans only. See Credit Union for full details. </p>
+
+		            	<p><a href="pages/auto-loans.php" class="biss-btn biss-btn-border-primary">learn more</a>
+
+	              </div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+		<!-- Layerslider end //-->
+
+		<!-- Site footer start //-->
+
+		<footer class="site-footer">
+
+			<?php include 'inc/footer.php';?>
+
+		</footer>
+
+		<!-- Site footer end //-->
+
+		<!-- Scroll to the top button start //-->
+
+		<div id="scroll-to-the-top">
+
+			<i class="fa fa-angle-up"></i>
+
+		</div>
+
+		<!-- Scroll to the top button end //-->
+
+	</div>
+
+<!-- JS includes //-->
+<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/less-1.7.4.min.js"></script>
+<script src="js/jquery.colorbox-min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.rivathemes.js"></script>
+<script src="js/layerslider/jquery-easing-1.3.js" type="text/javascript"></script>
+<script src="js/layerslider/jquery-transit-modified.js" type="text/javascript"></script>
+<script src="js/layerslider/layerslider.transitions.js" type="text/javascript"></script>
+<script src="js/layerslider/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+<script src="js/biss.js"></script>
+
+<script>
+(function($) {
+	"use strict";
+
+	//
+	// Preload images
+	//
+
+	$('document').ready(function() {
+
+		var $container = $('body'),
+			tweetsTimer;
+
+		$container.imagesLoaded( function() {
+
+			$('#layerslider').layerSlider({
+				skinsPath               : 'css/layerslider/skins/',
+				skin : 'fullwidth',
+				hoverPrevNext : true,
+				responsive : true,
+				responsiveUnder : 1170,
+				sublayerContainer : 1170
+			});
+			
+			$('#layerslider2').layerSlider({
+				skinsPath               : 'css/layerslider/skins/',
+				skin : 'fullwidth',
+				firstLayer: 2,
+				thumbnailNavigation : 'hover',
+				hoverPrevNext : true,
+				responsive : true,
+				responsiveUnder : 1170,
+				sublayerContainer : 1170
+			});
+
+
+		});
+
+		clearTimeout(tweetsTimer);
+
+	});
+
+})(jQuery);
+</script>
+ <?php include 'inc/modals.php';?>
+<script src="remodal/dist/remodal.js"></script>
+</body>
+</html>
